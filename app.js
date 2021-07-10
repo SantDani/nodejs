2f4a90c685df875851135248c3d628da4312c73a
SantiDani <profsantiRP@gmail.com>
2021-07-10 13:08:42 +0200

    Routing and Middleware

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
-const {fruits, money} = require('./fruits');
+const express = require("express");
+const app = express();
+const port = 3000;
 
-fruits.forEach((fruit) => {
-  console.count(fruit);
+
+
+// Routing
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 
-console.log('actual money', money);
+app.use(express.static(__dirname + "/public"));
 
-var cowsay = require("cowsay");
-console.log(
-    cowsay.say({
-      text: "I'm a moooodule",
-      e: "OwO",
-      T: ":P",
-    })
-);
-
-const http = require("http");
-
-const port = 8000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Hello, World!</h1>");
+app.get("/contacto", (req, res) => {
+    res.send("ruta de contacto" + __dirname);
 });
 
-server.listen(port, () => {
-  console.log(`Server running at port ${port}`);
+app.use((req, res, next) => {
+    // res.status(404).send("Sorry cant find that!");
+    res.status(404).sendFile(__dirname + "/public/404.html");
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
