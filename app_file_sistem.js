f682eed3e61f11c0c148395d930130429810b9a3
SantiDani <profsantiRP@gmail.com>
2021-07-29 17:42:30 +0200

    25 - stream reading and stream writing

--- app_file_sistem.js
+++ app_file_sistem.js
@@ -7,15 +7,22 @@ const buffer = 16 * 1024;
 // asynchronous call
 const steam_reading = fs.createReadStream(file, {encoding: 'utf8', highWaterMark: buffer});
 
+const file_result = __dirname + '/data/output_console.txt';
+
+const stream_writing = fs.createWriteStream(file_result);
+
 /**
- * this Call back reading, It's notify when is read 16kb
+ * this Call back reading, It's notify when is read 16kb and buffer is full for write in output_console.txt
  */
 
 steam_reading.on('data', function (buffer){
-    console.log(buffer.length + '************************************************************');
-    console.log(buffer.length + '************************************************************');
-    console.log(buffer.length + '************************************************************');
-    console.log(buffer.length + '************************************************************');
+    // console.log(buffer.length + '************************************************************');
+    // console.log(buffer.length + '************************************************************');
+    // console.log(buffer.length + '************************************************************');
+    // console.log(buffer.length + '************************************************************');
     console.log(buffer);
+    stream_writing.write(buffer);
 })
 console.log('I print first because reaFile is asynchronous call');
+
+
