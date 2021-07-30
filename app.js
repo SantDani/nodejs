bcc0fc33dfbb8681568433b42832a81cfb76efd2
profSanti <profsantirp@gmail.com>
2021-07-30 16:16:53 +0200

    36 Express and 1r server

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+'use strict'
+
 const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', function(request, response) {
+    response.send(`
+        <html>
+            <head></head>
+            <body>
+                <p>Welcome to Home!</p>
+            </body>
+        </html>
+    `);
+});
+
+app.listen(port);
