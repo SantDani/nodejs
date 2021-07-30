dfa8e289d784780effdffa09442ca7cd2f88ca0a
profSanti <profsantirp@gmail.com>
2021-07-30 16:52:26 +0200

    38 - Middleware - static file .css

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 'use strict'
 
 const express = require('express');
+const port = process.env.PORT || 3000;
 
 const app = express();
-const port = process.env.PORT || 3000;
+
+// When search 'assets' it's redirection to 'static' folder that contain the CSS
+app.use('/css', express.static(__dirname + '/static/css'));
+
 
 app.get('/champ/:id/', function(req, res) {
     res.send(`
         <html>
-            <head></head>
+            <head>
+                <!-- Import main.css-->
+                <link rel="stylesheet" href="/css/main.css" />
+            </head>
             <body>
                 El campeon es ${req.params.id}
             </body>
